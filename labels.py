4fc2cdd3e0876de00974527b739cb690721c3692
SharmaVibha <35629922+SharmaVibha@users.noreply.github.com>
2020-10-12 12:28:17 +0530

    Add files via upload

---
+++ labels.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def dv(dv):
+    if dv == 1:
+        label = 1
+    else:
+        label = 0
+    return label
+
+def dv1(dv1):
+    if dv1 == 2:
+        label = 1
+    else:
+        label = 0
+    return label
+
+
+# In[ ]:
+
+
+
+
